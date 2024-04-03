0f50de4462cde5932e060344a835079505c0476a
bigdust <3264085417@qq.com>
2024-04-04 00:38:55 +0800

    feat: ginx

--- pkg/ginx/middleware/ratelimit/builder.go
+++ pkg/ginx/middleware/ratelimit/builder.go
@@ -42,10 +42,7 @@ func (b *Builder) Build() gin.HandlerFunc {
 			log.Println(err)
 			// 这一步很有意思，就是如果这边出错了
 			// 要怎么办？
-			// 保守做法：因为借助于 Redis 来做限流，那么 Redis 崩溃了，为了防止系统崩溃，直接限流
 			ctx.AbortWithStatus(http.StatusInternalServerError)
-			// 激进做法：虽然 Redis 崩溃了，但是这个时候还是要尽量服务正常的用户，所以不限流
-			// ctx.Next()
 			return
 		}
 		if limited {
