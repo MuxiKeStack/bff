db6c4bafd84765fe47c06e584ae796f405abf595
bigdust <3264085417@qq.com>
2024-04-23 20:44:56 +0800

    fix: 修改成易懂的字段

--- web/search/search_vo.go
+++ web/search/search_vo.go
@@ -4,17 +4,17 @@ type SearchReq struct {
 	// 需要指定biz，如course，以后拓展可以增加不指定biz的逻辑
 	Biz            string `form:"biz"`
 	Keyword        string `form:"keyword"`
-	SearchLocation string `form:"search_location"` // 可以是 "home" 或 "favorites"
+	SearchLocation string `form:"search_location"` // 可以是 "Home" 或 "Collections"
 }
 
 type DeleteHistoryReq struct {
-	SearchLocation string  `json:"search_location"` // 可以是 "home" 或 "favorites"
-	RemoveAll      bool    `json:"remove_all"`
-	HistoryIds     []int64 `json:"history_ids"`
+	SearchLocation   string  `json:"search_location"` // 可以是 "Home" 或 "Collections"
+	RemoveAll        bool    `json:"remove_all"`
+	RemoveHistoryIds []int64 `json:"remove_history_ids"`
 }
 
 type GetHistoryReq struct {
-	SearchLocation string `form:"search_location"` // 可以是 "home" 或 "favorites"
+	SearchLocation string `form:"search_location"` // 可以是 "Home" 或 "Collections"
 }
 
 type HistoryVo struct {
