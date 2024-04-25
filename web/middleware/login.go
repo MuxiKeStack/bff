88a17480a9e2e4069d7defd94a4d9abf9eb6cea2
bigdust <3264085417@qq.com>
2024-04-25 23:57:57 +0800

    fix: 设置可受限制访问需要token的路由的paths

--- web/middleware/login.go
+++ web/middleware/login.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/MuxiKeStack/bff/web/ijwt"
+	"github.com/ecodeclub/ekit/set"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
@@ -9,68 +11,82 @@ import (
 )
 
 type LoginMiddlewareBuilder struct {
+	allowRestrictedAccessPaths set.Set[string]
 	ijwt.Handler
 }
 
 func NewLoginMiddleWareBuilder(hdl ijwt.Handler) *LoginMiddlewareBuilder {
-	return &LoginMiddlewareBuilder{
-		Handler: hdl,
+	s := set.NewMapSet[string](3)
+	s.Add("/evaluations/list/recent")
+	l := &LoginMiddlewareBuilder{
+		allowRestrictedAccessPaths: s,
+		Handler:                    hdl,
 	}
+	return l
+}
+
+func (m *LoginMiddlewareBuilder) allowRestrictedAccess(path string) bool {
+	if m.allowRestrictedAccessPaths.Exist(path) {
+		return true
+	}
+	parts := strings.Split(strings.Trim(path, "/"), "/")
+	// todo check
+	if len(parts) == 3 && parts[0] == "evaluations" && parts[2] == "detail" {
+		return true
+	}
+	return false
 }
 
 func (m *LoginMiddlewareBuilder) Build() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		authCode := ctx.GetHeader("Authorization")
-		// 没token
-		if authCode == "" {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
+		// 放行游客可访问的路由，
+		uc, err := m.extractUserClaimsFromAuthorizationHeader(ctx)
+		if err == nil {
+			ctx.Set("user", uc)
+		} else {
+			if m.allowRestrictedAccessPaths.Exist(ctx.Request.URL.Path) {
+				ctx.Set("user", uc)
+			} else {
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+			}
 		}
-		// Bearer xxxx
-		segs := strings.Split(authCode, " ")
-		if len(segs) != 2 {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		tokenStr := segs[1]
-		uc := ijwt.UserClaims{}
-		token, err := jwt.ParseWithClaims(tokenStr, &uc, func(*jwt.Token) (interface{}, error) {
-			// 可以根据具体情况给出不同的key
-			return m.JWTKey(), nil
-		})
-		if err != nil {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		if token == nil || !token.Valid {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		// token有效
-		// User-Agent
-		if uc.UserAgent != ctx.GetHeader("User-Agent") {
-			// 大概率是攻击者才会进入这个分支
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		ok, err := m.CheckSession(ctx, uc.Ssid)
-		if err != nil || ok {
-			// err如果是redis崩溃导致，考虑进行降级，不再验证是否退出 refresh_token降级的话收益会很少，因为是低频接口
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-		// 刷新过期时间,固定每分钟只会刷一次
-		//expireAt := uc.ExpiresAt.Time
-		//if expireAt.Sub(time.Now()) < time.Minute*29 {
-		//	// 刷新
-		//	uc.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute * 30))
-		//	tokenStr, err = token.SignedString(ijwt.JWTKey)
-		//	if err != nil {
-		//		// 刷新失败，但是校验已成功，不应该影响正常访问
-		//		log.Println(err)
-		//	}
-		//	ctx.Header("x-ijwt-token", tokenStr)
-		//}
-		ctx.Set("user", uc)
 	}
+}
+
+func (m *LoginMiddlewareBuilder) extractUserClaimsFromAuthorizationHeader(ctx *gin.Context) (ijwt.UserClaims, error) {
+	authCode := ctx.GetHeader("Authorization")
+	// 没token
+	if authCode == "" {
+		return ijwt.UserClaims{}, errors.New("authorization为空")
+	}
+	// Bearer xxxx
+	segs := strings.Split(authCode, " ")
+	if len(segs) != 2 {
+		return ijwt.UserClaims{}, errors.New("authorization为空格式不合理")
+	}
+	tokenStr := segs[1]
+	uc := ijwt.UserClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, &uc, func(*jwt.Token) (interface{}, error) {
+		// 可以根据具体情况给出不同的key
+		return m.JWTKey(), nil
+	})
+	if err != nil {
+		return ijwt.UserClaims{}, err
+	}
+	if token == nil || !token.Valid {
+		return ijwt.UserClaims{}, errors.New("token无效")
+	}
+	// token有效
+	// User-Agent
+	if uc.UserAgent != ctx.GetHeader("User-Agent") {
+		// 大概率是攻击者才会进入这个分支
+		return ijwt.UserClaims{}, errors.New("User-Agent验证：不安全")
+	}
+	ok, err := m.CheckSession(ctx, uc.Ssid)
+	if err != nil || ok {
+		// err如果是redis崩溃导致，考虑进行降级，不再验证是否退出 refresh_token降级的话收益会很少，因为是低频接口
+		// 这里 != nil 就是异常，可能崩溃，或连不上
+		return ijwt.UserClaims{}, errors.New("session检验：失败")
+	}
+	return uc, nil
 }
